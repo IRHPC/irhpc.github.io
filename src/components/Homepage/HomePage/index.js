0b733a1e4b0bdcaafa9524203d127318456764a0
SiggiD <54945695+SiggiDet@users.noreply.github.com>
2023-07-12 10:23:48 +0000

    Updated the first comment in Homepage to display free user support

--- src/components/Homepage/HomePage/index.js
+++ src/components/Homepage/HomePage/index.js
@@ -4,10 +4,10 @@ import HomepageFeatures from "../Features";
 
 const HTMLcomment1 =<> 
 <strong>I</strong>celandic <strong>H</strong>igh <strong>P</strong>erformance <strong>C</strong>omputing Centre (IHPC/IREI) 
-provides researchers with facilities to model
+provides researchers with free user support and facilities to model
 complex and difficult real world problems by 
 providing hardware consisting of powerful CPUS, GPUS, 
-fast I/O networking, and data storage solutions. </>
+fast I/O networking, data storage solutions. </>
 
 const HTMLcomment2 = <>IHPC/IREI is the Icelandic Research e-Infrastructure project, 
                      funded by the Icelandic Centre of Research infrastructure fund </>
