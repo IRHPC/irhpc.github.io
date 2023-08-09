88122ded7ef7a61a84cef82bb7a223a618d40136
siggiDet <sigurdurdet@gmail.com>
2023-08-09 16:02:31 +0000

    Announcement on the Homepage now implemented, add the latest announcements both to the announcemnt directory and src/components/HomePage/MessageItems/Announcements/data.json

--- src/components/Homepage/Messages/MessageItems/Announcements/index.js
+++ src/components/Homepage/Messages/MessageItems/Announcements/index.js
@@ -7,39 +7,38 @@ const sec = 1000;
 const type = "announce";
 
 const Announcements = () => {
-  // Convert the Blogs object into an array of blog posts
   const blogArray = Object.keys(Blogs).map(key => Blogs[key]);
 
   const currentDate = new Date();
   const twoWeeksAgo = new Date();
-  twoWeeksAgo.setDate(currentDate.getDate() - 14);
+  twoWeeksAgo.setDate(currentDate.getDate() - 14); // Change this to 14 days (2 weeks)
 
   const recentBlogsWithinTwoWeeks = blogArray.filter(blog => {
     const blogDate = new Date(blog.date);
     return blogDate >= twoWeeksAgo;
   });
-  
+
   if (recentBlogsWithinTwoWeeks.length === 0) {
     return null;
   }
-  
+
   let count = 0;
   const waitTime = 0.75 * sec; // 0.75 seconds wait time for each bubble
-  
+
   return (
     <section>
       {recentBlogsWithinTwoWeeks.map(blog => (
         <MessageBubble
-          key={blog.date} // Use blog.date instead of latestBlogWithinWeek.date
-          text={blog.content} // Use blog.content
+          key={blog.date}
+          text={blog.content}
           wait={count++ * waitTime}
           title={blog.title}
-          date={blog.date}
           type={type}
+          link={blog.link}
         />
       ))}
     </section>
   );
-}  
+}
 
 export default Announcements;
--- src/components/Homepage/Messages/MessageItems/MessageBubble/index.js
+++ src/components/Homepage/Messages/MessageItems/MessageBubble/index.js
@@ -3,9 +3,8 @@ import styles from './index.modules.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 
-
-const iconSizes='2x';
-const iconColors='#FFFFF';
+const iconSizes = '2x';
+const iconColors = '#FFFFFF'; // Corrected value here
 
 function MessageBubble({ text, title, wait, type }) {
   const style = {
@@ -22,7 +21,7 @@ function MessageBubble({ text, title, wait, type }) {
     <section className={className} style={style}>
       {type === 'announce' ? (
         <div className="announcement-content">
-          <FontAwesomeIcon icon={faCircleExclamation } size={iconSizes} color={iconColors} />
+          <FontAwesomeIcon icon={faCircleExclamation} size={iconSizes} color={iconColors} />
           <h3>Announcement: {title}</h3>
           <p className="text"><b>Description:</b> <br/> {text}</p>
         </div>
@@ -36,4 +35,3 @@ function MessageBubble({ text, title, wait, type }) {
 }
 
 export default MessageBubble;
-
